ad184d9e2da3c37e5c9d05c4937d15d0b94a79eb
aliperez <alicia.perez@utexas.edu>
2018-03-30 11:54:35 -0700

    got the pin from user search to appear on map, WOW!

--- Backend/logicBackend.js
+++ Backend/logicBackend.js
@@ -1,12 +1,14 @@
 // // Celeste's key just in case we need it later...
 // var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=290c7176&app_key=8e151b01fb2a821c90c46ee1498a8215&search=" + userInput + "&rated=true&minRating=0&maxRating=4";
 
+var geoCodingAddress, lat, long;
+
 
 $("#submit-button").on("click", function (event) {
 
     event.preventDefault();
 
-    var userInput = $("#inputField").val();
+    var userInput = $("#inputField").val(); 
 
     var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=b3e49cae&app_key=9895f628abd6b37aff48c8eab486f7ed&search=" + userInput + "&rated=true&minRating=0&maxRating=4";
 
@@ -28,16 +30,62 @@ $("#submit-button").on("click", function (event) {
         console.log(response[0].websiteURL);
 
         console.log("Rating: " + response[0].currentRating.rating);
+
+        console.log(response[1].mailingAddress.streetAddress1 + ", " + response[1].mailingAddress.city + ", " + response[1].mailingAddress.stateOrProvince + ", " + response[1].mailingAddress.postalCode);
+
+        geoCodingAddress = response[1].mailingAddress.streetAddress1 + ",+" + response[1].mailingAddress.city + ",+" + response[1].mailingAddress.stateOrProvince;
+
+        // console.log(geoCodingAddress);
+
+        // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo
+
+        // https://maps.googleapis.com/maps/api/geocode/json?address=14175+Southwest+Galbreath+Drive,+Sherwood,+OR&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo
+
+        $.ajax({
+            url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + geoCodingAddress + "&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo",
+            method: "GET"
+        }).then(function (response) {
+
+            // console.log("Inner AJAX response: " + geoCodingAddress);
+
+            // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo
+
+            // https://maps.googleapis.com/maps/api/geocode/json?address=14175+Southwest+Galbreath+Drive,+Sherwood,+OR&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo
+
+            console.log("Geocoding response latitude: " + JSON.stringify(response.results[0].geometry.location.lat));
+            console.log("Geocoding response longitude: " + JSON.stringify(response.results[0].geometry.location.lng));
+
+            lat = response.results[0].geometry.location.lat;
+            long = response.results[0].geometry.location.lng;
+
+            myMap();
+
+        });
+
     });
 
     $("#inputField").val("");
 
 });
 
+
+function myMap() {
+    var myCenter = new google.maps.LatLng(lat, long);
+
+    var mapCanvas = document.getElementById("map");
+
+    var mapOptions = { center: myCenter, zoom: 5 };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({ position: myCenter });
+    marker.setMap(map);
+}
+
+
+
 // // W3 Schools no marker
 
 // function myMap() {
-    
+
 //     var mapProp= {
 //         center:new google.maps.LatLng(51.508742,-0.120850),
 //         zoom:5,
@@ -49,16 +97,7 @@ $("#submit-button").on("click", function (event) {
 
 // // W3 Schools with marker
 
-function myMap() {
-    var myCenter = new google.maps.LatLng(20, -116);
 
-    var mapCanvas = document.getElementById("map");
-
-    var mapOptions = {center: myCenter, zoom: 5};
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({position:myCenter});
-    marker.setMap(map);
-  }
 
 
 
