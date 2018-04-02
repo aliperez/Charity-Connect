e1d293c057a72a1873f0967c8ff67d02069cb51e
aliperez <alicia.perez@utexas.edu>
2018-04-02 13:38:42 -0700

    updated front-back.js

--- Frontend/Template/js/front-back.js
+++ Frontend/Template/js/front-back.js
@@ -1,19 +1,14 @@
 var map;
-
 $(document).ready(function () {
-
     $(document).bind('keypress', function (e) {
         if (e.keyCode == 13) {
             $('#submit-button').trigger('click');
         }
     });
-
     $("#submit-button").on("click", function (event) {
         event.preventDefault();
-
         var userInput = $("#inputField").val();
-        var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=b3e49cae&app_key=9895f628abd6b37aff48c8eab486f7ed&search=" + userInput + "&rated=true&minRating=0&maxRating=4&pageSize=3";
-
+        var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=b3e49cae&app_key=9895f628abd6b37aff48c8eab486f7ed&search=" + userInput + "&rated=true&minRating=0&maxRating=4";
         $.ajax({
             url: queryURL,
             method: "GET",
@@ -27,39 +22,33 @@ $(document).ready(function () {
         // empty the input field
         $("#inputField").val("");
     });
-
     // $('#submit-button').on('click', function() { CELESTE
     //     if ($(this).hasClass('clicked')) {
     //         myMap(null);
     //         console.log("cool");
     //      }
     //    });
-
-
 });
 function myMap() {
-    // var lat = 50;
-    var lat = 45;
-    // var long = 14;
-    var long = -95;
+    var lat = 50;
+    var long = 14;
     var myCenter = new google.maps.LatLng(lat, long);
     var mapCanvas = document.getElementById("map");
-    var mapOptions = { center: myCenter, zoom: 3.5};
+    var mapOptions = { center: myCenter, zoom: 2 };
     map = new google.maps.Map(mapCanvas, mapOptions);
 }
-
 function updateResult(data) {
     for (var key in data) {
         // console.log(data[key].websiteURL);
         var contentString = '<div class="info-window">' + '<h5>' + data[key].charityName + '</h5>' +
             '<div class="info-content">' + '<strong>Rating: </strong>' + data[key].currentRating.rating + '<br>' + '<strong>Address: </strong>' + data[key].mailingAddress.streetAddress1 + ", " + data[key].mailingAddress.city + ", " + data[key].mailingAddress.stateOrProvince + ", " + data[key].mailingAddress.postalCode + '<br>' + "<strong>Tag line: </strong>" + data[key].tagLine + '<br>' + '<strong>Website: </strong>' + "<a href='" + data[key].websiteURL + "' target='_blank'>" + data[key].websiteURL + '</a>';
         // console.log(contentString);
-
         var geoCodingAddress = data[key].mailingAddress.streetAddress1 + "," + data[key].mailingAddress.city + "," + data[key].mailingAddress.stateOrProvince;
         // console.log(contentString);
         $.ajax({
             url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + geoCodingAddress + "&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo",
             method: "GET",
+            // sync ajax request instead of async works but browser freezes!! 
             async: false,
             success: function (data) {
                 // contentString overrides !!! 
@@ -72,59 +61,41 @@ function updateResult(data) {
         })
     }
 }
-function updateLatLong(data, latLongContentString) {
+function updateLatLong(data, contentString) {
     // console.log(contentString);
     // console.log(data);
     if (data.status == "OK") {
         var lat = JSON.stringify(data.results[0].geometry.location.lat);
         var long = JSON.stringify(data.results[0].geometry.location.lng);
         //console.log(lat, long)
-
         var newPin = new google.maps.LatLng(lat, long);
         var marker = new google.maps.Marker({ position: newPin, map: map });
+        marker.setMap(map);
         var infowindow = new google.maps.InfoWindow({
-         content:'<div class="scrollFix">'+contentString+'</div>',
-         maxWidth: 400
+            content:'<div class="scrollFix">'+contentString+'</div>',
+            maxWidth: 400
         });
-
-
-
         currentWindow = null;
-
-
-
-
         marker.addListener('click', function () {
             if (currentWindow) currentWindow.close();
             infowindow.open(map, marker);
             currentWindow=infowindow;
         });
-
         google.maps.event.addListener(map, 'click', function () {
             if
             (infowindow != null) { infowindow.close(); }
         });
-
-    } 
-}
-
-
-
     }
 }
-
 // $("#delete-markers").on("click", function(){ CELESTE 
 //     console.log("working");
 //     myMap(null);
 // });
-
 // function clearMarkers() { CELESTE
 //     myMap(null);
 //   }
-
 // function deleteMarkers() { CELESTE
 //     clearMarkers();
 //     var marker = new google.maps.Marker({ position: myCenter });
 //     marker.setMap(map);
 //   }
-
