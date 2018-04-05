52136aea4be599a51e16fa2cae6190aa14496a60
nkand84 <nityabhagi@gmail.com>
2018-04-04 17:33:11 -0700

    adding deployed site image and updated readme

--- js/logic.js
+++ js/logic.js
@@ -115,7 +115,7 @@ function updateResult(data, callback) {
 
     // Geocoding ajax call takes in address as a string and spits out lat/long coordinates
     $.ajax({
-        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + geoCodingAddress + "&key=AIzaSyDf5_ufIVYnnt4x6mjVhaVwXUncIyIRGxo",
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + geoCodingAddress + "&key=AIzaSyBWuBE5DwrQDFu98fYvZDbGhwqDv1dE9ps",
         method: "GET",
         success: function (response) {
 
